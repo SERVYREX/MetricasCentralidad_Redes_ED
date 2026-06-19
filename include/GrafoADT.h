3603a233964866769f44b10d0ebadb3dcfad0e0c
SERVYREX <codbryxp@gmail.com>
2026-06-18 20:30:17 -0400

    Se crearon las primeras funciones de la implementacion

--- include/GrafoADT.h
+++ include/GrafoADT.h
@@ -1,5 +1,7 @@
 #include <vector>
-
+#include <map>
+#include <algorithm>
+#include <stdexcept>
 //Usamos templates para generalizar nuestra implementacion
 
 //Estructura arista
@@ -52,8 +54,82 @@ public:
   virtual int getOutGrado(const TipoVertice& v) const = 0;
 };
 
- 
+// Implementacion basada en Listas de adyacencia
 
+template<typename TipoVertice, typename TipoPeso = int>
+
+class GrafoAdList : public Grafo<TipoVertice , TipoPeso>{
+  //Variable para controlar el tipo de grafo y creacion de lista de adyacencia mediante un mapa
+private:
+  bool dirigido;
+  int numAristas = 0;
+  std::map<TipoVertice, std::vector<Arista<TipoVertice,TipoPeso>>> listaAdyacencia;
+
+public:
+  GrafoAdList(bool esDirigido = false){
+    this->dirigido = esDirigido;
+  }
+  bool esDirigido() const override {
+    return dirigido;
+  }
+
+  void addVertice(const TipoVertice& v) override {
+    //Comprobamos que el vertice a insertar no este en el grafo, en caso de no estar, lo insertamos
+    if(listaAdyacencia.find(v) == listaAdyacencia.end()) {
+      listaAdyacencia.insert({v , std::vector<Arista<TipoVertice, TipoPeso>>()
+	});
+    }
+  }
+
+  void removeVertice(const TipoVertice& v) override {
+    //Si el vertice no se encuentra en el grafo, no hacemos nada
+    if(listaAdyacencia.find(v) == listaAdyacencia.end()) return;
+
+    //Elimina las aristas que apunten a v desde otros vertices
+
+    for (auto& par: listaAdyacencia){
+      if (par.first == v) continue;
+      auto& aristas = par.second;
+
+      for(auto itr = aristas.begin(); itr != aristas.end();){
+	if(itr-> destino == v){
+	  itr = aristas.erase(itr);
+	  numAristas--;
+	}
+	else {
+	  ++itr;
+	}
+      }
+    }
+    //Si el grafo es dirigido tambien hay que desconectar las aristas salientes
+    if(dirigido){
+      numAristas -= listaAdyacencia[v].size();
+    }
+    listaAdyacencia.erase(v);
+  }
+
+  
+  void addArista(const TipoVertice& v, const TipoVertice& u, TipoPeso peso) override {
+    //Comprobamos que los vertices esten en el grafo, en caso contrario los agregamos
+    addVertice(v);
+    addVertice(u);
+    //Agregamos la representacion de arista a la lista de adyacencia
+    listaAdyacencia[v].push_back({v,u,peso});
+    //En caso de no ser dirigido, creamos la arista pero de forma contraria
+    if(!dirigido){
+      listaAdyacencia[u].push_back({u,v,peso});
+    }
+    numAristas++;
+  }
+
+  int getNumVertices() const override {
+    return listaAdyacencia.size();
+  }
+  
+  int getNumAristas() const override {
+    return numAristas;
+  }
+  
  
   
 
